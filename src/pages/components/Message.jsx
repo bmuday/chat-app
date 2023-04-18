7cefb12f17d431c6e96101adc9c9186876127b62
bmuday <bmuday@live.fr>
2023-04-19 00:07:46 +0200

    check again

--- src/pages/components/Message.jsx
+++ src/pages/components/Message.jsx
@@ -1,11 +1,11 @@
 export default function Message({ message }) {
   console.log("message", message);
-  // const { text, createdAt, username } = message;
+  const { text, createdAt, username } = message;
   return (
     <div>
       <p>Message</p>
-      {/* <p>{text}</p>
-      <p>{createdAt}</p> */}
+      <p>{text}</p>
+      <p>{createdAt}</p>
     </div>
   );
 }
