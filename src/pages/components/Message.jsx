b1e4e9bb0bafbd89fe0e1ea885a92a2127d5d172
bmuday <bmuday@live.fr>
2023-04-19 00:10:25 +0200

    check message

--- src/pages/components/Message.jsx
+++ src/pages/components/Message.jsx
@@ -1,11 +1,12 @@
 export default function Message({ message }) {
-  console.log("message", message);
-  const { text, createdAt, username } = message;
   return (
     <div>
-      <p>Message</p>
-      <p>{text}</p>
-      <p>{createdAt}</p>
+      {message && (
+        <>
+          <p>{message.text}</p>
+          <p>{message.createdAt}</p>
+        </>
+      )}
     </div>
   );
 }
